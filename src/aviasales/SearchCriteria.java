bde55f4497076a043029ddc397b3645a5d5cea90
innopolis <alexanderermakov@protonmail.com>
2016-07-21 17:14:10 +0300

    update search criteria

--- src/aviasales/SearchCriteria.java
+++ src/aviasales/SearchCriteria.java
@@ -1,7 +1,20 @@
 package aviasales;
 
+import java.util.Date;
+
 /**
  * Created by laptop on 21.07.2016.
  */
+
 public class SearchCriteria {
+    String dept;
+    String arv;
+    Date dateDept;
+    Date dateArv;
+    SearchCriteria(String newDept, String newArv, Date newDateDept, Date newDateArv){
+        dept = newDept;
+        arv = newArv;
+        dateDept = newDateDept;
+        dateArv = newDateArv;
+    }
 }
