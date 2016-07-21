3dbe409bc36d0847279b6ebd0182065bfc69337d
Olzhas Aitkaliyev <olzhas.aitkaliyev@gmail.com>
2016-07-21 21:28:50 +0300

    Ticket Passenger improve

---
+++ src/aviasales/domain/Passenger.java
@@ -0,0 +1,21 @@
+package aviasales.domain;
+
+/**
+ * Created by laptop on 21.07.2016.
+ */
+public class Passenger {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Passenger(String name) {
+        this.name = name;
+    }
+
+}
--- src/aviasales/domain/Ticket.java
+++ src/aviasales/domain/Ticket.java
@@ -1,12 +1,43 @@
 package aviasales.domain;
 
+import java.time.LocalDate;
+
 /**
  * Created by laptop on 21.07.2016.
  */
 public class Ticket {
+    private LocalDate arrivalDate;
+    private LocalDate departureDate;
+    private City arrivalCity;
+    private City departureCity;
+    private Passenger passenger;
 
-    public Ticket(){
+    public City getArrivalCity() {
+        return arrivalCity;
+    }
 
+    public City getDepartureCity() {
+        return departureCity;
+    }
+
+    public LocalDate getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public LocalDate getDepartureDate() {
+        return departureDate;
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public Ticket(LocalDate arrivalDate, LocalDate departureDate, City arrivalCity, City departureCity, Passenger passenger){
+        this.arrivalCity = arrivalCity;
+        this.departureCity = departureCity;
+        this.arrivalDate = arrivalDate;
+        this.departureDate = departureDate;
+        this.passenger = passenger;
     }
 
 }
