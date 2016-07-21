f0faf8a769d91e27ecbc5845425eac7a7b67ded5
Olzhas Aitkaliyev <olzhas.aitkaliyev@gmail.com>
2016-07-21 22:09:09 +0300

    City improve

---
+++ src/aviasales/Main.java
@@ -0,0 +1,17 @@
+package aviasales;
+
+import java.text.ParseException;
+
+/**
+ * Created by laptop on 21.07.2016.
+ */
+public class Main {
+
+    public static void main(String [] args) throws ParseException {
+        UserInterface userInterface = new UserInterface();
+        userInterface.createUserInput();
+        //TestDijkstraAlgorithm a = new TestDijkstraAlgorithm();
+        //a.testExcute();
+    }
+
+}
