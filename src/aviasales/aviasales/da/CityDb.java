0b147f7b40e3cdb661553af2b5f783ca40aebd0f
Olzhas Aitkaliyev <olzhas.aitkaliyev@gmail.com>
2016-07-21 22:30:45 +0300

    City improve

--- src/aviasales/aviasales/da/CityDb.java
+++
@@ -1,8 +0,0 @@
-package aviasales.da;
-
-/**
- * Created by laptop on 21.07.2016.
- */
-public interface CityDb {
-    String getCityFromAirPort(String airportName);
-}
--- src/aviasales/aviasales/da/Route.java
+++
@@ -1,8 +0,0 @@
-package aviasales.da;
-
-/**
- * Created by neg on 7/21/16.
- */
-public class Route {
-
-}
