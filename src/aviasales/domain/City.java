4d216ab5eae30bcdf7c9ee3d0e40c1f0c5ebe71a
Olzhas Aitkaliyev <olzhas.aitkaliyev@gmail.com>
2016-07-21 21:49:45 +0300

    City improve

--- src/aviasales/domain/City.java
+++ src/aviasales/domain/City.java
@@ -1,11 +1,32 @@
 package aviasales.domain;
 
+import java.util.ArrayList;
+
 /**
  * Created by laptop on 21.07.2016.
  */
 public class City {
 
     private String cityName;
+
+    private ArrayList<Airport> airports;
+
+    public ArrayList<Airport> getAirports() {
+        return airports;
+    }
+
+    public void setAirports(ArrayList<Airport> airports) {
+        this.airports = airports;
+    }
+
+    public Airport getAirportByName(String airportName) {
+        for(int i = 0; i < airports.size(); i++) {
+            if(airports.get(i).getAirportName().equals(airportName)) {
+                return airports.get(i);
+            }
+        }
+        return null;
+    }
 
     public City(String cityName) {
         this.setCityName(cityName);
