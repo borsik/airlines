171e6d2bbf523d8af9b7896c1b323b68b5c4f2b9
innopolis <alexanderermakov@protonmail.com>
2016-07-21 20:00:38 +0300

    Dijkstra Algo

--- src/aviasales/da/FlightConcreteDB.java
+++ src/aviasales/da/FlightConcreteDB.java
@@ -15,6 +15,7 @@ public class FlightConcreteDB implements FlightDb {
     @Override
     public List<Flight> getFlightsByDate(Date flightDateFrom, Date flightDateTo) {
         final String fileType = ".csv";
-        final String delimiter ",";
+        final String delimiter = ",";
+        return null;
     }
 }
